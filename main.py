e9ca277bbd9c1af95758187bfe52b9ce49e66453
ian <highmeme10@gmail.com>
2022-03-01 13:54:22 +0300

    Test Split Example

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from sklearn import datasets
 import numpy as np
+from sklearn.model_selection import train_test_split
 
 iris = datasets.load_iris()
 
@@ -10,5 +11,11 @@ y = iris.target
 print(X.shape)
 print(y.shape)
 
-#Hours of study vs good/bad grades
 
+#Split and train using only 20% of the data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+print(X_train.shape)
+print(X_test.shape)
+print(y_train.shape)
+print(y_test.shape)
