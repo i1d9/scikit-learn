824ff0eca4b87613511beca54c684e2719ab3f81
ian <highmeme10@gmail.com>
2022-03-01 18:03:06 +0300

    Kmeans

--- kmeans.py
+++ kmeans.py
@@ -1,3 +1,4 @@
+from statistics import mode
 from sklearn.datasets import load_breast_cancer
 from sklearn.cluster import KMeans
 from sklearn.model_selection import train_test_split
@@ -6,4 +7,22 @@ from sklearn.preprocessing import scale
 import pandas as pd
 
 bc = load_breast_cancer()
-print(bc)
+
+X = scale(bc.data)
+y = bc.target
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+model = KMeans(n_clusters=2, random_state=0)
+
+#We dont pass y_train because in clustering y is the output not the input
+model.fit(X_train)
+
+predictions = model.predict(X_test)
+lables = model.labels_
+
+print("labels", lables)
+print("predictions", predictions)
+print("accuracy", accuracy_score(y_test, predictions))
+print("actual", y_test)
+
+print(pd.crosstab(y_train, lables))
