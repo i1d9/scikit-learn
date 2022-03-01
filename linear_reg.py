1d1bec777d61104a81faadf73f34117d7f8ab74e
ian <highmeme10@gmail.com>
2022-03-01 15:12:53 +0300

    Liner Regression Completed

--- linear_reg.py
+++ linear_reg.py
@@ -8,6 +8,25 @@ boston = datasets.load_boston()
 #Features
 X = boston.data
 #Lables
-Y = boston.target
+y = boston.target
 
 print(f"X\n{X}\n{X.shape}\n")
+
+
+#Algorithm
+l_reg = linear_model.LinearRegression()
+
+plt.scatter(X.T[5], y)
+plt.show()
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+#Train the model
+model = l_reg.fit(X_train, y_train)
+predictions = model.predict(X_test)
+
+print("Predictions: ", predictions)
+print("R^2 value: ", l_reg.score(X, y))
+print("coedd: ", l_reg.coef_)
+print("intercept: ", l_reg.intercept_)
